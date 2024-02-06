fac6168183cf434623b7db6e0dbed38528b9d840
briangor <briangor13@gmail.com>
2024-02-06 17:18:10 +0300

    refactor to async await syntax

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express(); // create an instance of the express module (app is the c
 const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
-app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
+// older promise api 
+/* app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
   fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
     .then(res => res.json()) // return a promise containing the response
     .then(json => res.send(`
@@ -17,7 +18,28 @@ app.get('/', (req, res) => { // send a get request to root directory ('/' is thi
     .catch((err) => { // catch any errors
       console.log(err); // log errors to the console
     })
-})
+}) */
+
+const func = async (req, res) => {
+  try {
+    const response = await fetch('https://www.boredapi.com/api/activity');
+    const json = await response.json();
+    res.send(
+      `
+    <div style="text-align: center;
+    margin-top: 25vh;
+    ">
+    <h1>Today's Activity</h1>
+    <p style="font-weight: bold; font-size: 1.5rem">${json.activity}!</p>
+    </div>
+    `
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+app.get('/', func);
 
 app.listen(PORT, () => { // start server and listen on specified port
   console.log(`App is running on ${PORT}`) // confirm server is running and log port to the console
